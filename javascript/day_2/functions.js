225f63589ba48997491b1a4dc99c3cd687501a62
nullsploit01 <harshaldharmik099@gmail.com>
2025-05-29 09:36:40 +0530

    day 3

--- javascript/day_2/functions.js
+++ javascript/day_2/functions.js
@@ -15,28 +15,43 @@
 // let d = 0;
 // addAndPrint(d, 15);
 
-function toThePower(number, exponent) {
-  let result = 1;
+// function toThePower(number, exponent) {
+//   let result = 1;
 
-  for (let i = 0; i < exponent; i += 1) {
-    result *= number;
-  }
+//   for (let i = 0; i < exponent; i += 1) {
+//     result *= number;
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
 // const res = toThePower(4, 0);
 // console.log(res);
 
-const toThePower = (number, exponent) => {
-  let result = 1;
+// const toThePower = (number, exponent) => {
+//   let result = 1;
 
-  for (let i = 0; i < exponent; i += 1) {
-    result *= number;
+//   for (let i = 0; i < exponent; i += 1) {
+//     result *= number;
+//   }
+
+//   return result;
+// };
+
+// const res = toThePower(4, 3);
+// console.log(res);
+
+const isPalindrome = (string) => {
+  // return true if its same as reversed string
+
+  const lengthOfString = string.length;
+  let reversedString = "";
+
+  for (let i = lengthOfString - 1; i >= 0; i -= 1) {
+    reversedString += string[i];
   }
 
-  return result;
+  return reversedString === string;
 };
 
-const res = toThePower(4, 3);
-console.log(res);
+console.log(isPalindrome("level"));
